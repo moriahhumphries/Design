4cb0150c333e0dc94b064967ef1ee2bf20b76014
Moriah Humphries <moriah.humphries@gmail.com>
2020-07-12 18:29:44 -0500

    testing materialize dropdown options

--- JS/moriah-js.js
+++ JS/moriah-js.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log(M);
     var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems);
+    var instances = M.Dropdown.init(elems, "hover");
+    // var instance = M.Dropdown.getInstance(elem);
+
 
 });
 
