36c3180208034444882e62ca0c8935c993b8362a
Moriah Humphries <moriah.humphries@gmail.com>
2020-07-12 18:46:41 -0500

    updated page title and JS

--- JS/moriah-js.js
+++ JS/moriah-js.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log(M);
     var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, "hover");
+    var instances = M.Dropdown.init(elems, M.hover);
     // var instance = M.Dropdown.getInstance(elem);
 
 
 });
+
 
 
 M.AutoInit();
