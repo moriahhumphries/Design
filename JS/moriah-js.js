4f615026554279b654046cca93377809f871e9a9
Moriah Humphries <moriah.humphries@gmail.com>
2020-07-13 12:29:24 -0500

    Added popup forms for login and join on mobile

--- JS/moriah-js.js
+++ JS/moriah-js.js
@@ -1,17 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log(M);
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {
-        hover: true
-    })
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log(M);
+//     var elems = document.querySelectorAll('.dropdown-trigger');
+//     var instances = M.Dropdown.init(elems, {
+//         hover: true
+//     })
+//
+//     // var instance = M.Dropdown.getInstance(elem);
+//
+//
+// });
+//
+//
+//
+// M.AutoInit();
 
-    // var instance = M.Dropdown.getInstance(elem);
+function openLoginForm() {
+    document.getElementById("login-form").style.display = "block";
+}
 
+function closeLoginForm() {
+    document.getElementById("login-form").style.display = "none";
+}
 
-});
+function openJoinForm() {
+    document.getElementById("join-form").style.display = "block";
+}
 
-
-
-M.AutoInit();
-
+function closeJoinForm() {
+    document.getElementById("join-form").style.display = "none";
+}
 
