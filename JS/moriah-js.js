61bd05e62fb31dba4532c23493064b2a0b6298d4
Moriah Humphries <moriah.humphries@gmail.com>
2020-07-13 11:50:32 -0500

    added fixed attachment to background image on mobile index page

--- JS/moriah-js.js
+++ JS/moriah-js.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log(M);
     var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options)
+    var instances = M.Dropdown.init(elems, {
+        hover: true
+    })
 
     // var instance = M.Dropdown.getInstance(elem);
 
