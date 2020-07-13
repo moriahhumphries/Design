7925ab6f80b687dad2fc27c437e69df5c79bcf93
Moriah Humphries <moriah.humphries@gmail.com>
2020-07-13 14:12:45 -0500

    Added signup windows to the desktop page

--- JS/moriah-js.js
+++ JS/moriah-js.js
@@ -15,18 +15,36 @@
 // M.AutoInit();
 
 function openLoginForm() {
-    document.getElementById("login-form").style.display = "block";
+    document.getElementById("small-login-form").style.display = "block";
 }
 
 function closeLoginForm() {
-    document.getElementById("login-form").style.display = "none";
+    document.getElementById("small-login-form").style.display = "none";
 }
 
 function openJoinForm() {
-    document.getElementById("join-form").style.display = "block";
+    document.getElementById("small-join-form").style.display = "block";
 }
 
 function closeJoinForm() {
-    document.getElementById("join-form").style.display = "none";
+    document.getElementById("small-join-form").style.display = "none";
+}
+
+// Large
+
+function openLargeLoginForm() {
+    document.getElementById("large-login-form").style.display = "block";
+}
+
+function closeLargeLoginForm() {
+    document.getElementById("large-login-form").style.display = "none";
+}
+
+function openLargeJoinForm() {
+    document.getElementById("large-join-form").style.display = "block";
+}
+
+function closeLargeJoinForm() {
+    document.getElementById("large-join-form").style.display = "none";
 }
 
