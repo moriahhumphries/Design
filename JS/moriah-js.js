ec48ed4a59c140ff8c3d042429898fc4ee191fd2
Moriah Humphries <moriah.humphries@gmail.com>
2020-07-12 19:35:14 -0500

    updated login button and forms

--- JS/moriah-js.js
+++ JS/moriah-js.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log(M);
     var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, M.hover);
+    var instances = M.Dropdown.init(elems, options)
+
     // var instance = M.Dropdown.getInstance(elem);
 
 
